bb9c73e999025fdcef03caae7e13779db9190411
Idowu <iarifayan@russelsmith.local>
2019-08-30 14:51:34 +0100

    Integrated sendgrid

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const sgMail = require("@sendgrid/mail");
 const app = express();
+const cors = require("cors");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,17 +17,27 @@ app.get("/", (req, res) => {
 });
 
 app.post("/mail", (req, res) => {
+  sgMail.s;
   const msg = {
     to: req.body.email,
-    from: "Sent <noreply@russelsmithgroup.com>",
-    subject: "This is a test, don't be alarmed",
-    text: "Welcome to API playground",
-    html: "<strong>Welcome to API playground</strong>"
+    from: "test@example.com",
+    subject: "Sending with Twilio SendGrid is Fun",
+    templateId: "d-b623f995f4b346fca48fd2925821808f",
+    dynamic_template_data: {
+      subject: "Sending with Twilio SendGrid is Fun",
+      name: req.body.username,
+      sender_name: "stizzleB",
+      sender_address: "10, Owonikoko Blvd, Ajah, Lagos."
+    }
+    // text: "and easy to do anywhere, even with Node.js",
+    // html: "<strong>and easy to do anywhere, even with Node.js</strong>"
   };
-  sgMail
-    .send(msg)
-    .then(res => res.status(200).json({ success: true, msg: res }))
-    .catch(err => res.status(500).json({ success: false, err }));
+  return sgMail.send(msg, (err, result) => {
+    if (err) {
+      res.status(500).json({ success: false, err });
+    }
+    res.status(200).send({ success: true, result });
+  });
 });
 
 app.listen(PORT, (req, res) =>
