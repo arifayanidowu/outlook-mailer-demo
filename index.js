37b2287e84be4622bd1b03f06f9ad4455a80ee8c
Idowu <iarifayan@russelsmith.local>
2019-08-30 09:08:43 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -22,74 +22,74 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-const transport = nodemailer.createTransport({
-  host: "smtp.office365.com",
-  port: 587,
-  secureConnection: false,
-  tls: {
-    rejectUnauthorized: false,
-    ciphers: "SSLv3"
-  },
-  auth: {
-    user: "rssmtp@russelsmithgroup.com",
-    pass: "Nigeria*2"
-  }
-});
-
-app.post("/mail", (req, res) => {
-  const testEmail = "noreply@russelsmithgroup.com";
-  const options = {
-    from: testEmail,
-    to: req.body.email,
-    subject: "Hello from test app",
-    text: `Hello ${req.body.email}, You've just received an email from ${testEmail}`
-  };
-  transport.sendMail(options, (err, info) => {
-    if (err) {
-      res.status(500).json({ success: false, msg: `Failure to send email` });
-    }
-    res.status(200).json({
-      success: true,
-      infoId: info.messageId
-    });
-    transport.close();
-  });
-});
+// const transport = nodemailer.createTransport({
+//   host: "smtp.office365.com",
+//   port: 587,
+//   secureConnection: false,
+//   tls: {
+//     rejectUnauthorized: false,
+//     ciphers: "SSLv3"
+//   },
+//   auth: {
+//     user: "rssmtp@russelsmithgroup.com",
+//     pass: "Nigeria*2"
+//   }
+// });
 
 // app.post("/mail", (req, res) => {
-//   nodeoutlook.sendEmail({
-//     auth: {
-//       user: "rssmtp@russelsmithgroup.com",
-//       pass: "Nigeria*2"
-//     },
-//     tls: {
-//       rejectUnauthorized: false,
-//       ciphers: "SSLv3"
-//     },
-//     // secure: true,
-//     requireTLS: true,
-//     from: "iarifayan@russelsmithgroup.com",
+//   const testEmail = "noreply@russelsmithgroup.com";
+//   const options = {
+//     from: testEmail,
 //     to: req.body.email,
-//     subject: `Test email to ${req.body.email}`,
-//     html: `<h3>This is just a test, so don't be alarmed</h3>`,
-//     onError: e => console.log(e),
-//     onSuccess: i => res.status(200).send({ msg: i })
-//   });
-// sendmail(
-//   {
-//     from: "arifayanidowu@gmail.com",
-//     to: req.body.email,
-//     replyTo: "iarifayan@russelsmithgroup.com",
-//     subject: "Mail to test sendmail package",
-//     html: `This is just a test so don't be alarmed`
-//   },
-//   (err, reply) => {
+//     subject: "Hello from test app",
+//     text: `Hello ${req.body.email}, You've just received an email from ${testEmail}`
+//   };
+//   transport.sendMail(options, (err, info) => {
 //     if (err) {
-//       res.json(err);
+//       res.status(500).json({ success: false, msg: `Failure to send email` });
 //     }
-//     res.status(200).json(reply);
-//   }
-// );
+//     res.status(200).json({
+//       success: true,
+//       infoId: info.messageId
+//     });
+//     transport.close();
+//   });
 // });
+
+app.post("/mail", (req, res) => {
+  nodeoutlook.sendEmail({
+    auth: {
+      user: "rssmtp@russelsmithgroup.com",
+      pass: "Nigeria*2"
+    },
+    tls: {
+      rejectUnauthorized: false,
+      ciphers: "SSLv3"
+    },
+    // secure: true,
+    requireTLS: true,
+    from: "iarifayan@russelsmithgroup.com",
+    to: req.body.email,
+    subject: `Test email to ${req.body.email}`,
+    html: `<h3>This is just a test, so don't be alarmed</h3>`,
+    onError: e => console.log(e),
+    onSuccess: i => res.status(200).send({ msg: i })
+  });
+  // sendmail(
+  //   {
+  //     from: "arifayanidowu@gmail.com",
+  //     to: req.body.email,
+  //     replyTo: "iarifayan@russelsmithgroup.com",
+  //     subject: "Mail to test sendmail package",
+  //     html: `This is just a test so don't be alarmed`
+  //   },
+  //   (err, reply) => {
+  //     if (err) {
+  //       res.json(err);
+  //     }
+  //     res.status(200).json(reply);
+  //   }
+  // );
+});
 
 app.listen(PORT, () => console.log(`[Server]: running live on port ${PORT}`));
